1fd926913b94c420dd6db949c02cc3161cad3fdc
Tedros Kahsu <tedkahcom@gmail.com>
2024-02-14 16:01:44 +0300

    Clean up setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Card, Image, Heading, Text } from "rebass";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box width={256}>
+        <Card p={1} borderRadius={2} boxShadow="0 0 16px rgba(0, 0, 0, .25)">
+          <Image
+            src={`https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20`}
+          />
+          <Box px={2}>
+            <Heading as="h3">Card</Heading>
+            <Text fontSize={0}>Small meta text</Text>
+          </Box>
+        </Card>
+      </Box>
     </div>
   );
 }
